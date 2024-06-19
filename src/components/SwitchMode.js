a0cd3cc776a029bde6f27ebf85f7893598c9707d
anguswong <angus.wong@gmail.com>
2024-06-19 11:56:30 +0800

    Swtich completed

--- src/components/SwitchMode.js
+++ src/components/SwitchMode.js
@@ -1,7 +1,36 @@
-const SwitchMode = () => {
-    return (
-        <div className="switchMode">SwitchMode</div>
-    );
-  };
-  
-  export default SwitchMode;
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Switch from '../components/Switch';
+
+const Row = styled.div`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Label = styled.span`
+  margin-left: 8px;
+  font-size: 20px;
+  color: ${(props) => props.theme.color};
+`;
+
+const SwitchMode = ({ label, isActive, onClick }) => {
+  return (
+    <Row>
+      <Switch
+        isActive={isActive}
+        onClick={onClick}
+      />
+      <Label>{label}</Label>
+    </Row>
+  );
+};
+
+SwitchMode.propTypes = {
+  label: PropTypes.string,
+  isActive: PropTypes.bool,
+  onClick: PropTypes.func
+};
+
+export default SwitchMode;
